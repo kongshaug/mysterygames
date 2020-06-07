7606bd8e15b682f76719b29d78caa37151c828f8
Amalie Landt <malie@hotmail.dk>
2020-06-07 20:20:53 +0200

    small edit

--- src/main/java/entities/EntityFactoryImpl.java
+++ src/main/java/entities/EntityFactoryImpl.java
@@ -12,7 +12,6 @@ import entities.interfaces.Riddle;
 import entities.interfaces.User;
 import errorhandling.NotFoundException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 import java.util.UUID;
